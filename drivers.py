0ccbf8ab94c72dde1499b300d3f17d11abfffba2
deepurala <pradeep.urala@outlook.com>
2023-08-17 12:06:30 +0100

    drivers.py finalised

--- drivers.py
+++ drivers.py
@@ -1,22 +1,19 @@
 import requests
-import json
+import pandas as pd
  
 URL = "http://ergast.com/api/f1/drivers.json"
- 
+
 limit = 100
 offset = 0
 drivers = []
 while True:
     PARAMS = {'limit':limit, 'offset': offset}
-
     r = requests.get(url = URL, params = PARAMS)
-
     data = r.json()
     for item in data['MRData']['DriverTable']['Drivers']:
         drivers.append(item)
     if offset > int(data['MRData']['total']):
         break
     offset += limit
-
-print(json.dumps(drivers))
+pd.DataFrame(drivers).to_csv('drivers.csv', index=False)
 
